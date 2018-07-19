6c51ba8c40ffac0a3f2c3fdc6d3d14f7ea6d0f87
idrein <irving.rein@duke.edu>
2018-07-19 18:22:27 -0400

    redesign with js, better fonts, and better css

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var map = {
+  "resume-link": "resume",
+  "writings-link": "writings",
+  "papers-link": "paper-review",
+  "email-link": "email",
+  "facebook-link": "facebook",
+  "linkedin-link": "linkedin",
+  "github-link": "github",
+};
+
+$(".link").each(function(index) {
+  $(this).hover(function() {
+    var key = $(this).attr('id');
+    $("#hello-link").text(map[key] + ". ");
+  }, function() {
+    $("#hello-link").text("hello.");
+  });
+});
